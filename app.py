38abfec1440e928ec7796d2bf1cc9c7c3aef20b1
tylerkero <112517094+tylerkero@users.noreply.github.com>
2022-11-08 04:13:22 -0500

    update

--- app.py
+++ app.py
@@ -14,28 +14,19 @@ app = Flask(__name__)
 def index():
     return render_template('form.html')
 
-# @app.route('/location',methods = ["POST",'GET'])
-# def location():
-#     if request.method == "Post":
-#         return find_stop_near()
 @app.route('/location/', methods = ["POST","GET"])
 def location():
     if request.method=="GET":
         return f" The url /location is accessed directly. Try going to / "
     if request.method == "POST":
         form_data = request.form
-        print(form_data)
+        #print(form_data)
         for value in form_data.values():
-            print(value)
+            #print(value)
             neareststop = find_stop_near(value)
         stop = neareststop[0]
         wheelchair = neareststop[1]
         return render_template('location.html', stop=stop, wheelchair = wheelchair )
-
-
-# @app.route('/<location>', methods = ["GET","POST"])
-# def showlocation(location):
-#     return f'Location: {find_stop_near(location)}'
 
 if __name__ == '__main__':
     app.run(debug=True)
